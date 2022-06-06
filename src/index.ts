dece7a89671e48f6d4b5ff80ae86704bcdcebb5d
PascualVera <pascualvera.994@gmail.com>
2022-06-06 13:24:55 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors);
+const port = 8080;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
+
+io.on("connection", socket => {
+	console.log("a user connected");
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
+});
+
+server.listen(port, () => {
+	console.log(`listening on *:${port}`);
+});
